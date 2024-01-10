c631bb858983486a3799790a6d465457f6d2d0d1
Sontag Maxime <maxime_sontag@ens.univ-artois.fr>
2024-01-10 16:33:48 +0100

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ export const Footer: React.FC = () => {
         <>
             <footer className="w-full text-center bg-white shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                 <div className="mt-4">
-                    <Link href="/"><span className="mb-5 text-xl text-gray-300 hover:bg-gray-700 sm:text-xl dark:text-gray-300 p-4">Accueil</span></Link>
-                    <Link href="CV_Sontag_maxime.pdf" target="_blank"><span className="mb-5 text-xl text-gray-300 hover:bg-gray-700 sm:text-xl dark:text-gray-300 p-4">CV</span></Link>
-                    <Link href="mailto:maximesontag@gmail.com"><span className="mb-5 text-xl text-gray-300 hover:bg-gray-700 sm:text-xl dark:text-gray-300 p-4">Contact</span></Link>
+                    <Link href="/"><span className="mb-5 text-xl text-white hover:bg-gray-700 sm:text-xl p-4">Accueil</span></Link>
+                    <Link href="CV_Sontag_maxime.pdf" target="_blank"><span className="mb-5 text-xl text-white hover:bg-gray-700 sm:text-xl p-4">CV</span></Link>
+                    <Link href="mailto:maximesontag@gmail.com"><span className="mb-5 text-xl text-white hover:bg-gray-700 sm:text-xl p-4">Contact</span></Link>
                     <p className="mb-5 text-base text-gray-400 sm:text-lg dark:text-gray-300 pt-6">© 2023 Sontag Maxime</p>
                 </div>
             </footer>
